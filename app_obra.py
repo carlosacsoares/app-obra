0c91ce2c9ef416bdbb9d8d76a9b06ec345364d3c
carlosacsoares <carlosacsoares@gmail.com>
2026-07-25 16:13:33 -0300

    Update app_obra.py

--- app_obra.py
+++ app_obra.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.markdown("""
     <style>
         /* Fixa o topo (Título e Seletor de Cômodo) */
-        div[data-testid="stVerticalBlock"] > div:has(div[data-testid="stdivider"]) {
+        div[data-testid="stVerticalBlock"] > div:has(div[data-testid="selectbox"]) {
             position: sticky;
             top: 2.8rem;
             background-color: white;
